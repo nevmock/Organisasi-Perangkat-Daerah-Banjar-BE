3e22ccc4a191d9f68334461296fd9c6b44b8a71f
kevinirawand <kevinid07@gmail.com>
2025-06-16 05:22:47 +0700

    bug: default draft

--- src/models/DateModel.ts
+++ src/models/DateModel.ts
@@ -5,7 +5,7 @@ const dateSchema = new mongoose.Schema({
     tanggal_mulai: { type: Date, required: true },
     tanggal_selesai: { type: Date, required: true },
     link_laporan_pdf: { type: String },
-    status_laporan: { type: String, enum: ['draft', 'final', 'revisi'], required: true }
+    status_laporan: { type: String, enum: ['draft', 'final', 'revisi'], required: true, default: 'draft' },
 }, { timestamps: true });
 
 export const DateModel = mongoose.model('Date', dateSchema);
