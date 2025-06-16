11b5b56b6a334806e384083009dcfe0befa46153
optimus9021 <ismaianugrah@gmail.com>
2025-06-17 06:58:12 +0700

    push

--- src/routes/doRoutes.ts
+++ src/routes/doRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { DoController } from '../controllers/DoController';
-import { uploadDokumentasi } from '../middlewares/uploadMiddleware';
+import { uploadEvidence } from '../middlewares/upload';
+import { IndikatorController } from '../controllers/IndikatorController';
 
 const router = express.Router();
 const controller = new DoController();
+const controller2 = new IndikatorController
 
 router.get('/', controller.getAll);
 router.get('/search', controller.search);
@@ -12,8 +14,17 @@ router.get('/getById/:id', controller.getById);
 
 router.post('/', controller.create);
 router.put('/:id', controller.update);
+
+router.delete('/:id/remove-evidence', async (req, res) => {
+    await controller2.removeEvidence(req, res);
+});
+
 router.delete('/:id', controller.delete);
 
-router.post('/:id/dokumentasi', uploadDokumentasi, controller.addDokumentasi);
+router.post(
+    '/:id/dokumentasi',
+    uploadEvidence.array('evidence', 10),
+    controller2.uploadEvidence
+);
 
 export default router;
