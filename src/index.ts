be4e115a012a4b56b8914fbc69c7388f018af704
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-06-20 12:56:21 +0700

    add console log for every incoming request

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  console.info(
+    `[${new Date().toISOString()}] Request hit: ${req.method} ${req.originalUrl}`
+  );
+  next();
+});
+
 // app.use(cors({
 //     origin: '*',
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
