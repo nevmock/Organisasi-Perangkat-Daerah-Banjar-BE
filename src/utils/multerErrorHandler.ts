2b166ae0f5ba8fdb7911b3dc13409ff689b2a989
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-06-26 00:25:41 +0700

    feat: add error handling file ext when upload file

--- src/utils/multerErrorHandler.ts
+++ src/utils/multerErrorHandler.ts
@@ -7,30 +7,31 @@ export function handleMulterError(
   res: Response,
   next: NextFunction
 ) {
-  if (err instanceof multer.MulterError) {
-    let message = 'Terjadi kesalahan saat mengunggah berkas.';
+    if (err instanceof multer.MulterError) {
+        let message = 'Terjadi kesalahan saat mengunggah berkas.';
 
-    switch (err.code) {
-    case 'LIMIT_FILE_SIZE':
-        message = 'Ukuran berkas terlalu besar. Silakan unggah berkas dengan ukuran yang sesuai.';
-        break;
-    case 'LIMIT_FILE_COUNT':
-        message = 'Terlalu banyak berkas diunggah. Silakan unggah sesuai jumlah yang diperbolehkan.';
-        break;
-    case 'LIMIT_UNEXPECTED_FILE':
-        message = 'Berkas tidak dikenali. Silakan periksa kembali nama field pada formulir unggahan Anda.';
-        break;
-    default:
-        message = err.message || message;
-        break;
-    }
+        switch (err.code) {
+            case 'LIMIT_FILE_SIZE':
+                message = 'Ukuran berkas terlalu besar. Silakan unggah berkas dengan ukuran yang sesuai.';
+                break;
+            case 'LIMIT_FILE_COUNT':
+                message = 'Terlalu banyak berkas diunggah. Silakan unggah sesuai jumlah yang diperbolehkan.';
+                break;
+            case 'LIMIT_UNEXPECTED_FILE':
+                message = 'Berkas tidak dikenali. Silakan periksa kembali nama field pada formulir unggahan Anda.';
+                break;
+            default:
+                message = err.message || message;
+                break;
+        }
 
-    return res.status(400).json({ message });
-
+        return res.status(400).json({ message });
+    } else if (err && err.name === 'MulterFileFilterError') {
+        return res.status(400).json({ message: err.message });
     } else if (err) {
-    return res.status(500).json({
-        message: 'Terjadi kesalahan pada server saat mengunggah berkas. Silakan coba lagi nanti.'
-    });
+        return res.status(500).json({
+            message: 'Terjadi kesalahan pada server saat mengunggah berkas. Silakan coba lagi nanti.'
+        });
     }
 
     next();
